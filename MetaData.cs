3a98ee1bdae8dbfe9f69e9232045a0e33ec91fc0
Illia Chaparyn <illia.chaparyn@nixsolutions.com>
2019-11-14 19:55:31 +0200

    Added data serialization class

---
+++ MetaData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrumAnalyzer
+{
+    [DataContract]
+    class MetaData
+    {
+        [DataMember]
+        String Duration { get; set; }
+        [DataMember]
+        DateTime DateOfCreation { get; set; }
+        [DataMember]
+        String originName { get; set; }
+    }
+}
